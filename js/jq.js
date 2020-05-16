a68004090c084f6e4b95b5a68d7e9ce1cd8cc04e
AJX9000 <54915296+AJX9000@users.noreply.github.com>
2020-05-16 00:28:42 -0500

    Add files via upload

---
+++ js/jq.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // home page - fade-in of banner picture
+    $(".homePhoto").delay(100).fadeIn(1500);
+    // home page - fade-in of tagline below banner picture
+    $(".homeTagline").delay(750).fadeIn(1000);
+
+    // fade-in of banner titles on the other pages
+    $(".menuTitle").delay(100).fadeIn(1000);
+    $(".gallTitle").delay(100).fadeIn(1000);
+    $(".aboutTitle").delay(100).fadeIn(1000);
+});
